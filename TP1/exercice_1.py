73bf2344800b89bf8d0c39e273b85df05f37dfb1
youssef baddi <baddiyoussef85@gmail.com>
2023-01-04 14:27:32 +0100

    exercice 1 TP 1

---
+++ TP1/exercice_1.py
@@ -0,0 +1,6 @@
+# Exercise 1
+# TP1
+#   Décrivez le plus clairement et le plus complètement possible ce qui se passe à chacune des trois lignes de l’exemple ci-dessous :
+# 1 >>> largeur = 20
+# 2 >>> hauteur = 5 * 9.3
+# 3 >>> largeur * hauteur
